b6666cc3fc9466e4946f2858552d84fa3c780874
arielgwapo <91606038+arielgwapo@users.noreply.github.com>
2025-08-18 20:42:11 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,13 @@
+<script>
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('https://cdn.jsdelivr.net/gh/YourUsername/my-blogger-pwa/sw.js')
+        .then(registration => {
+          console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        })
+        .catch(err => {
+          console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+  }
+</script>
